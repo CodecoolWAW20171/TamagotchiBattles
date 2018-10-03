cf1f0c875dd44a1d607c51913e944a9a4bf5278e
matixk <matixk73@gmail.com>
2018-10-03 10:21:15 +0200

    User controller with username returning adedd

---
+++ src/main/java/com/codecool/tamagotchi/controller/User.java
@@ -0,0 +1,17 @@
+package com.codecool.tamagotchi.controller;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+
+@RestController
+public class User {
+
+    @RequestMapping("/user")
+    public Object user(OAuth2Authentication authentication) {
+        LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication.getUserAuthentication().getDetails();
+        return properties.get("name");
+    }
+}
