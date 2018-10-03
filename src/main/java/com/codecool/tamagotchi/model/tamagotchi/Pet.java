8f2d1041dc8b6c53c91658b2a5cc9c5b9bdda02e
matixk <matixk73@gmail.com>
2018-10-03 14:55:26 +0200

    Pet updated with new Enumerations

--- src/main/java/com/codecool/tamagotchi/model/tamagotchi/Pet.java
+++ src/main/java/com/codecool/tamagotchi/model/tamagotchi/Pet.java
@@ -1,22 +1,24 @@
 package com.codecool.tamagotchi.model.tamagotchi;
 
 import com.codecool.tamagotchi.model.tamagotchi.enumerations.Action;
+import com.codecool.tamagotchi.model.tamagotchi.enumerations.Type;
+
+import javax.validation.constraints.NotNull;
 
 public class Pet {
+    @NotNull
     private String name;
-    private int exp = 0;
+    @NotNull
     private int attack;
-    private int defend;
+    @NotNull
+    private int defence;
+    @NotNull
     private int speed;
+    @NotNull
+    private Type type;
+    private int exp = 0;
     private double health = 100;
     private Action state;
-
-    public Pet(String name, int attack, int defend, int speed) {
-        this.name = name;
-        this.attack = attack;
-        this.defend = defend;
-        this.speed = speed;
-    }
 
     public String getName() {
         return name;
@@ -42,12 +44,12 @@ public class Pet {
         this.attack = attack;
     }
 
-    public int getDefend() {
-        return defend;
+    public int getDefence() {
+        return defence;
     }
 
-    public void setDefend(int defend) {
-        this.defend = defend;
+    public void setDefence(int defend) {
+        this.defence = defend;
     }
 
     public int getSpeed() {
@@ -56,6 +58,14 @@ public class Pet {
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 
     public double getHealth() {
@@ -72,5 +82,17 @@ public class Pet {
 
     public void setState(Action state) {
         this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return  "name: " + name + "\n" +
+                "type: " + type + "\n" +
+                "exp: " + exp + "\n" +
+                "attack: " + attack + "\n" +
+                "defence: " + defence + "\n" +
+                "speed: " + speed + "\n" +
+                "health: " + health + "\n" +
+                "state: " + state + "\n";
     }
 }
