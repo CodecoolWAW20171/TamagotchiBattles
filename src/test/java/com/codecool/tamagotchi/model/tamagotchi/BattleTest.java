1ff8dbb30b392208a3ada568c665bfd9fb39d153
grzban <grzegorz.banaszek@gmail.com>
2018-10-15 13:32:36 +0200

    test schema for battle class

---
+++ src/test/java/com/codecool/tamagotchi/model/tamagotchi/BattleTest.java
@@ -0,0 +1,162 @@
+package com.codecool.tamagotchi.model.tamagotchi;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BattleTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getOrder() {
+    }
+
+    @Test
+    public void makeAMove() {
+    }
+
+    @Test
+    public void checkPrimaryAttack() {
+    }
+
+    @Test
+    public void checkSecondaryAttack() {
+    }
+
+    @Test
+    public void getBattleResult() {
+    }
+
+    @Test
+    public void checkIfWon() {
+    }
+
+    @Test
+    public void addWinningString() {
+    }
+
+    @Test
+    public void goThroughTurn() {
+    }
+
+    @Test
+    public void setTurn() {
+    }
+
+    @Test
+    public void getTurn() {
+    }
+
+    @Test
+    public void getFirstPlayer() {
+    }
+
+    @Test
+    public void getSecondPlayer() {
+    }
+
+    @Test
+    public void getFirstPlayerHealthAtStartOfTurn() {
+    }
+
+    @Test
+    public void setFirstPlayerHealthAtStartOfTurn() {
+    }
+
+    @Test
+    public void getSecondPlayerHealthAtStartOfTurn() {
+    }
+
+    @Test
+    public void setSecondPlayerHealthAtStartOfTurn() {
+    }
+
+    @Test
+    public void getBattleFinalString() {
+    }
+
+    @Test
+    public void setBattleFinalString() {
+    }
+
+    @Test
+    public void getOrder1() {
+    }
+
+    @Test
+    public void makeAMove1() {
+    }
+
+    @Test
+    public void checkPrimaryAttack1() {
+    }
+
+    @Test
+    public void checkSecondaryAttack1() {
+    }
+
+    @Test
+    public void getBattleResult1() {
+    }
+
+    @Test
+    public void checkIfWon1() {
+    }
+
+    @Test
+    public void addWinningString1() {
+    }
+
+    @Test
+    public void goThroughTurn1() {
+    }
+
+    @Test
+    public void setTurn1() {
+    }
+
+    @Test
+    public void getTurn1() {
+    }
+
+    @Test
+    public void getFirstPlayer1() {
+    }
+
+    @Test
+    public void getSecondPlayer1() {
+    }
+
+    @Test
+    public void getFirstPlayerHealthAtStartOfTurn1() {
+    }
+
+    @Test
+    public void setFirstPlayerHealthAtStartOfTurn1() {
+    }
+
+    @Test
+    public void getSecondPlayerHealthAtStartOfTurn1() {
+    }
+
+    @Test
+    public void setSecondPlayerHealthAtStartOfTurn1() {
+    }
+
+    @Test
+    public void getBattleFinalString1() {
+    }
+
+    @Test
+    public void setBattleFinalString1() {
+    }
+}
