490d506b07b489bff369afa564ab49243cb6fb7a
matixk <matixk73@gmail.com>
2018-10-16 12:29:37 +0200

    UserController moved to user subfolder

--- src/main/java/com/codecool/tamagotchi/user/UserController.java
+++ src/main/java/com/codecool/tamagotchi/user/UserController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.LinkedHashMap;
 
 @RestController
-public class User {
+public class UserController {
 
     @RequestMapping("/user")
-    public Object user(OAuth2Authentication authentication) {
+    public String user(OAuth2Authentication authentication) {
         LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication.getUserAuthentication().getDetails();
-        return properties;
+        return (String) properties.get("name");
     }
 }
