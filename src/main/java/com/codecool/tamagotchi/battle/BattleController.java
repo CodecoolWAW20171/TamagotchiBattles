2d27adedaed1f510113c575a15492e79a6b36e15
matixk <matixk73@gmail.com>
2018-10-16 12:30:17 +0200

    Added BattleController

--- src/main/java/com/codecool/tamagotchi/battle/BattleController.java
+++ src/main/java/com/codecool/tamagotchi/battle/BattleController.java
@@ -1,23 +1,52 @@
 package com.codecool.tamagotchi.battle;
 
+import com.codecool.tamagotchi.enumerations.Action;
 import com.codecool.tamagotchi.pet.Pet;
+import com.codecool.tamagotchi.user.UserController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.LinkedHashMap;
 
 @Controller
 public class BattleController {
     Battle battle = new Battle();
 
-    @PostMapping("/createPet")
+    @PostMapping("/room")
     public String petSubmit(OAuth2Authentication authentication, @ModelAttribute Pet pet) {
-        pet.setName((String) ((LinkedHashMap<String, Object>) authentication.getUserAuthentication().getDetails()).get("name"));
-        System.out.println(pet.toString());
-        if (battle.getFirstPlayer() == null) battle.setFirstPlayer(pet);
-        else if (battle.getSecondPlayer() == null) battle.setSecondPlayer(pet);
-        return "pet";
+        String username = new UserController().user(authentication);
+
+        pet.setName(username);
+        if (battle.getFirstPlayer() == null) {
+            battle.setFirstPlayer(pet);
+        }
+        else if (battle.getSecondPlayer() == null) {
+            battle.setSecondPlayer(pet);
+        }
+        return "battle";
     }
+
+    @PostMapping("/room/action")
+    public void action(OAuth2Authentication authentication) {
+        String username = new UserController().user(authentication);
+
+        if (username.equals(battle.getFirstPlayer().getName())) {
+            System.out.println("First player attack");
+            battle.getFirstPlayer().setState(Action.ATTACK);
+        } else if (username.equals(battle.getSecondPlayer().getName())) {
+            System.out.println("Second player attack");
+            battle.getSecondPlayer().setState(Action.ATTACK);
+        }
+    }
+
+    @MessageMapping("/action")
+    @SendTo("/battle/log")
+    public BattleLog log(String message) throws Exception {
+        return new BattleLog(message);
+    }
+
+
 }
