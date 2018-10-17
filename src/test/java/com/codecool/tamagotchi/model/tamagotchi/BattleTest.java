fc3d81a0c5f747304b8c861c3d309f7fba2067c5
gb <grzegorz.banaszek@gmail.com>
2018-10-17 21:36:17 +0200

    Simple test for battle

--- src/test/java/com/codecool/tamagotchi/model/tamagotchi/BattleTest.java
+++ src/test/java/com/codecool/tamagotchi/model/tamagotchi/BattleTest.java
@@ -19,26 +19,29 @@ public class BattleTest {
     public void setUp() throws Exception {
         player1 = new Pet();
         player1.setName("Pet 1");
-        player1.setAttack(50);
-        player1.setDefence(50);
-        player1.setSpeed(70);
+        player1.setAttack(70);
+        player1.setDefence(80);
+        player1.setSpeed(50);
         player1.setType(Type.ROCK);
 
         player2 = new Pet();
         player2.setName("Pet 2");
-        player2.setAttack(50);
-        player2.setDefence(50);
-        player2.setSpeed(70);
+        player2.setAttack(60);
+        player2.setDefence(80);
+        player2.setSpeed(60);
         player2.setType(Type.PAPER);
     }
 
     @Test
     public void startBattle() {
-        player1.setState(Action.DEFEND);
-        player2.setState(Action.ATTACK);
         battle = new Battle(player1, player2);
 
+        player1.setState(Action.ATTACK);
+        player2.setState(Action.DEFEND);
         battle.startBattle();
+        Assert.assertEquals(100, player1.getHealth(), 0);
+        Assert.assertEquals(-7.5, player2.getHealth(), 0.1);
+
         player1.setState(Action.SECONDARY_ATTACK);
         player2.setState(Action.DEFEND);
         battle.startBattle();
