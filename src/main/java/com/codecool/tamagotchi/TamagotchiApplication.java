eeabd83fded5d1650951e1e3b84d1311ab2b21de
matixk <matixk73@gmail.com>
2018-10-18 11:27:02 +0200

    Index moved to templates

--- src/main/java/com/codecool/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/codecool/tamagotchi/TamagotchiApplication.java
@@ -2,12 +2,18 @@ package com.codecool.tamagotchi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class TamagotchiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TamagotchiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
 	}
 
 }
