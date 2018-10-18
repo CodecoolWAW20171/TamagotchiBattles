c69736f445eb68037258cec1f5085c08c942e47f
matixk <matixk73@gmail.com>
2018-10-18 11:36:08 +0200

    Added login site

--- src/main/java/com/codecool/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/codecool/tamagotchi/TamagotchiApplication.java
@@ -2,9 +2,12 @@ package com.codecool.tamagotchi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class TamagotchiApplication {
 
 	public static void main(String[] args) {
@@ -12,8 +15,9 @@ public class TamagotchiApplication {
 	}
 
 	@RequestMapping("/")
-	public String home() {
-		return "index";
+	public String home(OAuth2Authentication authentication) {
+		if (authentication != null) return "index";
+		return "login";
 	}
 
 }
--- src/main/resources/static/js/login/facebook.js
+++ src/main/resources/static/js/login/facebook.js
@@ -21,17 +21,3 @@ $.ajaxSetup({
     }
 });
 
-$.get("/user", function(data) {
-    let loginContainer = $(".containerUnauthenticated");
-    let actionBar = $("#actionBar");
-    if(data) {
-        loginContainer.hide();
-        actionBar.show();
-    }
-    else {
-        loginContainer.show();
-        actionBar.hide();
-    }
-});
-
-
